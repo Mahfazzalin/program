c44a7c53c1ae0d36f1445784894858699481b759
Mahfazzalin Shawon Reza <mahfazzalin1@gmail.com>
2023-07-28 10:20:44 +0600

    remove title bar, add fake title bar , move over the screen, wrong time shown error fixed.

--- python/testmahf.py
+++ python/testmahf.py
@@ -5,15 +5,37 @@ from datetime import datetime
 
 # creating tkinter window
 root = Tk()
-root.geometry("277x91")
+root.geometry("300x80")
 root.overrideredirect(True)  # remove title bar
 
 # Create a style object
 style = Style()
 
 # Set the background color for the Frame
-style.configure("Custom.TFrame", background="#454545")
+style.configure("Custom.TFrame", background="darkgreen")
 
+def mahftime():
+    dt = datetime.now()
+    hour = dt.strftime("%H")
+    minute = dt.strftime("%M:%S")
+
+    if int(hour) >= 7 and int(hour) <= 24:
+        mahfhour = int(hour) - 6
+        if mahfhour > 12:
+            mahfhour = mahfhour - 12
+            ltnt = "night"
+        else:
+            mahfhour = mahfhour
+            ltnt = "light"
+    else:
+        mahfhour = int(hour) + 24
+        mahfhour = mahfhour - 12
+        ltnt = "night"
+
+    time = str(mahfhour) + ":" + minute + " " + ltnt
+    string = time
+    lbl.config(text=string)
+    lbl.after(1000, mahftime)
 
 def move_app(e):
     root.geometry(f'+{e.x_root}+{e.y_root}')
@@ -24,19 +46,19 @@ def quitter(e):
 
 
 # create fake title bar
-title_bar = Frame(root, style="Custom.TFrame", relief="raised", border=0)
-title_bar.pack(expand=0, fill=X)
+title_bar = Frame(root, style="Custom.TFrame", relief="raised", border=1)
+title_bar.pack(expand=1, fill=X)
 
 # bind the title bar
 title_bar.bind("<B1-Motion>", move_app)
 
 # create title text
-title_label = Label(title_bar, text="MahfTime", background="#454545", foreground="white")
-title_label.pack(side=LEFT, pady=2)
+title_label = Label(title_bar, text="MahfTime", background="darkgreen", foreground="white")
+title_label.pack(side=LEFT, pady=4)
 
 # create close button on titleBar
-close_label = Label(title_bar, text="  X   ", background="#454545", foreground="white", relief="raised", border=0)
-close_label.pack(side=RIGHT, pady=0)
+close_label = Label(title_bar, text="  X   ", background="darkgreen", foreground="white", relief="raised", border=1)
+close_label.pack(side=RIGHT, pady=4)
 close_label.bind("<Button-1>", quitter)
 
 
@@ -63,7 +85,7 @@ def mahftime():
     time = str(mahfhour) + ":" + minute + " " + ltnt
     string = time
     lbl.config(text=string)
-    lbl.after(100, mahftime)
+    lbl.after(1000, mahftime)
 
 
 # Styling the label widget so that clock
--- python/time.py
+++ python/time.py
@@ -5,11 +5,12 @@ import time
 def mahftime():
     dt = datetime.now()
     hour= dt.strftime("%H")
+    print(hour)
     minute = dt.strftime("%M:%S")
 
-    if int(hour) >=6 and int(hour)<= 24 :
-        mahfhour = int(hour)-5
-        if mahfhour >12:
+    if int(hour) >=7 and int(hour)<= 24 :
+        mahfhour = int(hour)-6
+        if mahfhour>12:
             mahfhour = mahfhour-12
             ltnt="night"
         else:
@@ -17,7 +18,7 @@ def mahftime():
             ltnt= "light"
     else:
         mahfhour = int(hour)+24
-        mahfhour = mahfhour-12
+        mahfhour = mahfhour-18
         ltnt= "night"
     
     time = str(mahfhour)+":"+ minute+" "+ltnt
